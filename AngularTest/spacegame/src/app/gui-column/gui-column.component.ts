61c4948204eeb9eb968aa26d6be1fe25be555597
Sergei Yarema <isara.isara8@gmail.com>
2019-12-19 01:32:57 +0200

    Add template arguments for gui columns

--- AngularTest/spacegame/src/app/gui-column/gui-column.component.ts
+++ AngularTest/spacegame/src/app/gui-column/gui-column.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, TemplateRef} from '@angular/core';
 
 @Component({
   selector: 'app-gui-column',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gui-column.component.scss']
 })
 export class GuiColumnComponent implements OnInit {
+
+  @Input()
+  innerContentTemplate: TemplateRef<any>;
 
   constructor() { }
 
