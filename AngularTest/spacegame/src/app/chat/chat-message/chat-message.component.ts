5fc8e8a409e3c9f665d0e99304433d07876a16d9
SergySanJj <isara.isara8@gmail.com>
2019-12-21 22:15:04 +0200

    Add autoscrolling on new message

--- AngularTest/spacegame/src/app/chat/chat-message/chat-message.component.ts
+++ AngularTest/spacegame/src/app/chat/chat-message/chat-message.component.ts
@@ -1,19 +1,25 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-chat-message',
   templateUrl: './chat-message.component.html',
   styleUrls: ['./chat-message.component.scss']
 })
-export class ChatMessageComponent implements OnInit {
+export class ChatMessageComponent implements OnInit, AfterViewInit {
 
-  @Input()
-  message: string;
+  @Input() message: string;
+  container: HTMLElement;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.container = document.getElementById('messagesContainer');
+  }
+
+  ngAfterViewInit(): void {
+    this.container = document.getElementById('messagesContainer');
+    this.container.scrollTop = this.container.scrollHeight;
   }
 
 }
